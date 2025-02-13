35be848a49e9f2b20c99d56f0c4b24f710d37043
Pieter Naucke <554603@student.fontys.nl>
2025-02-13 13:45:20 +0100

    Alarm met foutmelding

--- Alarm.py
+++ Alarm.py
@@ -1,5 +1,8 @@
 print('Hoeveel keer moet het alarm afgaan?')
 z = input()
 
-for x in range(1, int(z)+1):
-    print(f"Alarm {x} !")
+if int(z) > 0:
+    for x in range(1, int(z)+1):
+        print(f"Alarm {x} !")
+else:
+  raise Exception("Sorry, Geen getal lager dan 1") 
